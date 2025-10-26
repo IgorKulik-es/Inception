41aa5921db2320eba762e6978a630d3081586a01
IgorKulik-es <igorkulik28@proton.me>
2025-10-27 00:54:44 +0100

    added wp settings

--- srcs/requirements/wordpress/wp-config.sh
+++ srcs/requirements/wordpress/wp-config.sh
@@ -3,5 +3,14 @@
 initialize_wordpress()
 {
 	wp core download
-	wp config create --dbname=${DB_NAME} --dbuser=${DB_USER_NAME} --dbpass=${DB_PASSWORD}
+	wp config create --dbname=${DB_NAME} --dbuser=${DB_USER_NAME} --dbpass=${DB_USER_PASSWORD} --dbhost=mariadb:3306
+	wp install \
+		--url=${DOMAIN_NAME} \
+		--title=${WP_TITLE} \
+		--admin-user=${WP_ADMIN_NAME} \
+		--admin-password=${WP_ADMIN_PASSWORD} \
+		--admin_email=${WP_ADMIN_EMAIL}
+	wp create user ${WP_AUTHOR_USER} ${WP_AUTHOR_EMAIL} \
+		--user_pass=${WP_AUTHOR_PASSWORD}
+		--role=author;
 }
