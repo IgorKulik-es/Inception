8f70878573b83d2f8150c5dff3f5c6babb62c642
IgorKulik-es <igorkulik28@proton.me>
2025-10-27 00:55:06 +0100

    refactored initialization

--- srcs/requirements/mariadb/mariadb_init.sh
+++ srcs/requirements/mariadb/mariadb_init.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 mariadbd &
+
 while ! nc -w 1 127.0.0.1 3306 &>/dev/null; do sleep 1 ; done ;
+
 mariadb -e "DELETE FROM mysql.user WHERE User='';"
 mariadb -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
 mariadb -e "DROP DATABASE IF EXISTS test;"
@@ -11,10 +13,5 @@ mariadb -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mariadb -e "CREATE USER IF NOT EXISTS '$DB_USER_NAME'@'%' IDENTIFIED BY '$DB_USER_PASSWORD';"
 mariadb -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER_NAME'@'%';"
 mariadb -e "FLUSH PRIVILEGES;"
-PID=`ps | grep mariadbd | head -n 1 | awk '{print $1}' | tr -d '[:space:]'`
-echo $PID > out
-kill -s SIGTERM "$PID"
-while nc -w 1 127.0.0.1 3306 &>/dev/null; do sleep 1 ; done ;
-echo goodbye
-sleep 4
-exec "$@"
+
+mysqladmin shutdown -u root
--- srcs/requirements/mariadb/mariadb_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-while nc -z 127.0.0.1 3306 &>/dev/null; do sleep 1 ; done ;
-exec mariadbd-safe
