5005584146ab63bbdd2e1dd647c2d1c0a3f4ef97
IgorKulik-es <igorkulik28@proton.me>
2025-11-03 16:49:54 +0100

    added volumes

--- srcs/requirements/wordpress/wp_init.sh
+++ srcs/requirements/wordpress/wp_init.sh
@@ -13,7 +13,8 @@ initialize_users()
 		--title=${WP_TITLE} \
 		--admin_user=${WP_ADMIN_NAME} \
 		--admin_password=${WP_ADMIN_PASSWORD} \
-		--admin_email=${WP_ADMIN_EMAIL}
+		--admin_email=${WP_ADMIN_EMAIL} \
+		--skip-email
 	wp user create  ${WP_AUTHOR_USER} ${WP_AUTHOR_EMAIL} \
 		--user_pass=${WP_AUTHOR_PASSWORD} \
 		--role=author
